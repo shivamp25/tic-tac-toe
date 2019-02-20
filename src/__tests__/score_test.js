543f2e49a87d58fec951d677686c6632d62e4ed6
Shivam <shivampadaliya25@gmail.com>
2019-02-19 22:26:03 -0500

    added third test case for Score component

--- src/__tests__/score_test.js
+++ src/__tests__/score_test.js
@@ -3,6 +3,7 @@ import Score from '../components/Score';
 import { shallow, mount } from 'enzyme';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Information from "../components/Information";
 enzyme.configure({ adapter: new Adapter() });
 
 describe('Score', () => {
@@ -15,6 +16,12 @@ describe('Score', () => {
     it('should mount to the DOM ', () => {
         const wrapper = mount(<Score computerScore ={5} userScore={4} />);
         expect(wrapper.find('.Score').hostNodes()).toHaveLength(1);
+    });
+
+    it('should be able to display the correct score from props', () => {
+        const wrapper = mount(<Score computerScore ={5} userScore={4} />);
+        expect(wrapper.find('.computerScore').hostNodes().text()).toBe(5);
+        expect(wrapper.find('.userScore').hostNodes().text()).toBe(4);
     });
 
 });
