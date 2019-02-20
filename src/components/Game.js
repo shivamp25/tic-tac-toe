02acc0ef7a3a379b5e44345c33621040ae94452d
Shivam <shivampadaliya25@gmail.com>
2019-02-20 12:08:10 -0500

    first test passed for Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,3 +3,20 @@ import WelcomeScreen from './WelcomeScreen';
 import Information from './Information';
 import Board from './Board';
 import Score from './Score';
+
+export default class Game extends Component {
+    constructor() {
+        super();
+        this.state = {
+          welcomeScreen: true
+        };
+    }
+
+    render() {
+        if (this.state.welcomeScreen) {
+            return (
+              <WelcomeScreen className="WelcomeScreen"/>
+            );
+        }
+    }
+}
