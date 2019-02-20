d6b58c14c80c767b2442cbd4da5da1c07fe5f354
Shivam <shivampadaliya25@gmail.com>
2019-02-19 22:16:08 -0500

    first test case passed for Score component

--- src/components/Score.js
+++ src/components/Score.js
@@ -2,3 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const DivContainer = styled.div`
+    margin: auto;
+    font-size: 30px;
+`;
+
+const Score = ({ computerScore, userScore }) => {
+    Score.propTypes = {
+        computerScore: PropTypes.number.isRequired,
+        userScore: PropTypes.number.isRequired
+    };
+
+    return (
+        <DivContainer className="Score" />
+    );
+};
+export default Score;
