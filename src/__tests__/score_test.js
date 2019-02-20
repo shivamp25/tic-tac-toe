00864914bcd57544543e332b8a4e10db46963691
Shivam <shivampadaliya25@gmail.com>
2019-02-19 22:20:44 -0500

    made correction in first and second test cases for Score component

--- src/__tests__/score_test.js
+++ src/__tests__/score_test.js
@@ -5,9 +5,16 @@ import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 enzyme.configure({ adapter: new Adapter() });
 
-describe('Information', () => {
+describe('Score', () => {
+
     it('should be selectable by class name ', () => {
         const container = shallow(<Score computerScore ={5} userScore={4} />);
         expect(container.is('.Score')).toBe(true);
     });
+
+    it('should mount to the DOM ', () => {
+        const wrapper = mount(<Score computerScore ={5} userScore={4} />);
+        expect(wrapper.find('.Score').hostNodes()).toHaveLength(1);
+    });
+
 });
