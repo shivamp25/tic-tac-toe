3593b7384b5c6857b24a95456aed4b5e321b9618
Shivam <shivampadaliya25@gmail.com>
2019-02-21 00:41:47 -0500

    update

--- src/__tests__/score_test.js
+++ src/__tests__/score_test.js
@@ -3,7 +3,6 @@ import Score from '../components/Score';
 import { shallow, mount } from 'enzyme';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Information from "../components/Information";
 enzyme.configure({ adapter: new Adapter() });
 
 describe('Score', () => {
